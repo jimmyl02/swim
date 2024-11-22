b76f0bdba83376e31563313a376c0ce323bf395f
jimmyl02 <jimmyli2002@gmail.com>
2024-11-21 17:08:19 -0800

    feat: setup swim server

--- src/swim/swim.go
+++ src/swim/swim.go
@@ -1,10 +1,19 @@
 package swim
 
-import "github.com/jimmyl02/swim/types"
+import (
+	"context"
+
+	"github.com/jimmyl02/swim/types"
+)
 
 // swimGRPCServer defines the gRPC server for SWIM
 type swimGRPCServer struct {
-	types.UnimplementedSwimServer
+	types.UnimplementedSwimNodeServer
+}
+
+// Ping responds if the node is alive and piggybacks gossip
+func (s *swimGRPCServer) Ping(ctx context.Context, req *types.PingRequest) (*types.AckResponse, error) {
+	return &types.AckResponse{}, nil
 }
 
 // SwimServer defines a single node in a SWIM cluster
